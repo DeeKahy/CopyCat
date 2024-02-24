6a47d35a3c2146b26a6d80a6490e4c92b3d33be8
Lennart Diego Kahn <lennartdiegokahn@Lennarts-MacBook-Pro.local>
2024-02-25 00:36:39 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+use clipboard::{ClipboardContext, ClipboardProvider};
+use walkdir::WalkDir;
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let mut clipboard_content = String::new();
+
+    'outer: for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+
+        // Skip if the path matches any blacklist item
+        for arg in &args {
+            if path.ends_with(arg) || path.to_str().map_or(false, |s| s.contains(arg)) {
+                continue 'outer;
+            }
+        }
+
+        if path.is_file() {
+            match read_file(path) {
+                Ok(contents) => {
+                    clipboard_content.push_str(&format!("{:?}\n```\n{}\n```\n\n", path, contents));
+                }
+                Err(_) => continue, // Skip non-text files or errors
+            }
+        }
+    }
+
+    ctx.set_contents(clipboard_content).unwrap();
+}
+
+fn read_file(path: &Path) -> Result<String, std::io::Error> {
+    let file = File::open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = Vec::new();
+    buf_reader.read_to_end(&mut contents)?;
+
+    match String::from_utf8(contents) {
+        Ok(text) => Ok(text),
+        Err(_) => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Not a UTF-8 file")),
+    }
+}
