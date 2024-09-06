27b679b8f7ccdb1d3fbf0b9f96fd230081fac4a9
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-09-06 15:52:25 +0200

    Update ccat.nix

--- ccat.nix
+++ ccat.nix
@@ -1,25 +1,22 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-}:
+{ lib, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   pname = "ccat";
-  version = "0.1.0"; # Update this as needed
+  version = "001"; 
 
   src = fetchFromGitHub {
     owner = "DeeKahy";
     repo = "CopyCat";
-    rev = "main"; # Or use a specific commit hash or tag
-    sha256 = "sha256-K5cs2/yGZNANjrSvMqEfH0aus/KQgM8V4AmXZCnqb6k="; # Replace with the correct hash
-  };
+    rev = "001"; 
+    sha256 = "sha256-zllxQifRMNEMa3RO5WKrwGAUf1xQg6YrQBzIHzy43F0="; # This hash may need to be updated 
+    };
 
-  cargoHash = "sha256-STpKWRTO6ahBgTsHZc47M3WIAonK9et4nYJvo0zrKPk="; # Set to empty string initially
-
+  cargoHash = "sha256-LYVhvq5l+PCZXW+elWi3zZFxLekgPn+plo4dybbLK9g="; # This hash may need to be updated 
+  
   meta = with lib; {
     description = "A utility to copy project tree contents to clipboard";
     homepage = "https://github.com/DeeKahy/CopyCat";
-    license = licenses.mit; # Adjust if your license is different
+    license = licenses.mit;
     maintainers = [ maintainers.deekahy ];
   };
 }
